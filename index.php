ae361e86f98e9fb18bc402bfc041c7b81ad8a3ec
RuslanMak <makarovemd@gmail.com>
2018-04-12 18:15:31 +0300

    version-4

--- index.php
+++ index.php
@@ -16,16 +16,55 @@ foreach ($countrys as $country => $animals) {
         if (strpos($animal, ' ')) {
 //            echo "<h2>$country</h2>";
 //            echo $animal;
-            echo "<h2>$country</h2>";
             list($word1[], $word2[]) = explode(' ', $animal);
-                shuffle($word2);
-            foreach ($word1 as $key => $word) {
-                array_shift($word1);
-                echo $word . " " . $word2[$key] . "\n";
-            }
+            list($countrys2[]) = explode(' ', $country);
         }
     }
 }
-?>
+
+shuffle($word2);
+
+$i = 0;
+
+foreach ($word1 as $key => $word) {
+    echo "<h2>$countrys2[$i]</h2>";
+    $i++;
+    echo $word . " " . $word2[$key] . "\n";
+}
+
+exit;
+
+
+
+
+//Version-1 -  It is not the best. because it has some problem
+$countrys = [
+    'Canada' => ['Canadian horse', 'American beaver'],
+    'Finland' => ['Brown bear', 'Holly blue'],
+    'Iraq' => ['Chukar partridge', 'Goat'],
+    'Japan' => ['Green pheasant', 'Carp'],
+    'Mexico' => ['Golden eagle', 'Jaguar', 'Vaquita'],
+];
+
+echo '<pre>';
+//print_r($countrys);
+
+foreach ($countrys as $country => $animals) {
+    foreach ($animals as $animal) {
+        if (strpos($animal, ' ')) {
+            list($word1[], $word2[]) = explode(' ', $animal);
+            break;
+        }
+    }
+}
+
+shuffle($word2);
+
+foreach ($word1 as $key => $word) {
+    $countrys2 = key($countrys);
+    echo "<h2>$countrys2</h2>";
+    next($countrys);
+    echo $word . " " . $word2[$key] . "\n";
+}
 
 
