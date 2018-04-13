d3824d119633fa834069c26da5f15af59d93e62e
RuslanMak <makarovemd@gmail.com>
2018-04-13 13:07:17 +0300

    version-5

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$countrys = [
-    'Canada' => ['Canadian horse', 'American beaver'],
+$countries = [
+    'Canada' => ['Canadian horse', 'North American beaver'],
     'Finland' => ['Brown bear', 'Holly blue'],
     'Iraq' => ['Chukar partridge', 'Goat'],
     'Japan' => ['Green pheasant', 'Carp'],
@@ -9,15 +9,16 @@ $countrys = [
 ];
 
 echo '<pre>';
-//print_r($countrys);
+//print_r($countries);
 
-foreach ($countrys as $country => $animals) {
+foreach ($countries as $country => $animals) {
     foreach ($animals as $animal) {
-        if (strpos($animal, ' ')) {
+        $space = substr_count($animal, " ");
+        if (strpos($animal, ' ') && $space < 2) {
 //            echo "<h2>$country</h2>";
 //            echo $animal;
             list($word1[], $word2[]) = explode(' ', $animal);
-            list($countrys2[]) = explode(' ', $country);
+            list($countries2[]) = explode(' ', $country);
         }
     }
 }
@@ -27,44 +28,8 @@ shuffle($word2);
 $i = 0;
 
 foreach ($word1 as $key => $word) {
-    echo "<h2>$countrys2[$i]</h2>";
+    echo "<h2>$countries2[$i]</h2>";
     $i++;
     echo $word . " " . $word2[$key] . "\n";
 }
-
-exit;
-
-
-
-
-//Version-1 -  It is not the best. because it has some problem
-$countrys = [
-    'Canada' => ['Canadian horse', 'American beaver'],
-    'Finland' => ['Brown bear', 'Holly blue'],
-    'Iraq' => ['Chukar partridge', 'Goat'],
-    'Japan' => ['Green pheasant', 'Carp'],
-    'Mexico' => ['Golden eagle', 'Jaguar', 'Vaquita'],
-];
-
-echo '<pre>';
-//print_r($countrys);
-
-foreach ($countrys as $country => $animals) {
-    foreach ($animals as $animal) {
-        if (strpos($animal, ' ')) {
-            list($word1[], $word2[]) = explode(' ', $animal);
-            break;
-        }
-    }
-}
-
-shuffle($word2);
-
-foreach ($word1 as $key => $word) {
-    $countrys2 = key($countrys);
-    echo "<h2>$countrys2</h2>";
-    next($countrys);
-    echo $word . " " . $word2[$key] . "\n";
-}
-
 
