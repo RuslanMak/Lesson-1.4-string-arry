6a2d7db7fd43f58158de0bf35aee08b2fe32778d
RuslanMak <makarovemd@gmail.com>
2018-04-17 12:48:34 +0300

    version-8

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ $countries = [
 ];
 
 echo '<pre>';
-//print_r($countries);
 
 $word1 = [];
 $word2 = [];
@@ -17,13 +16,9 @@ $countries2 = [];
 
 foreach ($countries as $country => $animals) {
     foreach ($animals as $animal) {
-        $space = substr_count($animal, " ");
-        if (strpos($animal, ' ') && $space < 2) {
-//            echo "<h2>$country</h2>";
-//            echo $animal;
+        if (substr_count($animal, " ") == 1) {
             list($word1[], $word2[]) = explode(' ', $animal);
             list($countries2[]) = explode(' ', $country);
-//            break;
         }
     }
 }
@@ -41,4 +36,47 @@ foreach ($word1 as $key => $word) {
     }
     $i++;
     echo $word . " " . $word2[$key];
+}
+
+exit;
+
+echo "<br><br><br>" . "-----------------------------------VERSION-2-------------------------------";
+
+$countries = [
+    'Canada' => ['Canadian horse', 'North American beaver'],
+    'Finland' => ['Brown bear', 'Holly blue'],
+    'Iraq' => ['Chukar partridge', 'Goat'],
+    'Japan' => ['Green pheasant', 'Carp'],
+    'Mexico' => ['Golden eagle', 'Jaguar', 'Vaquita'],
+];
+
+echo '<pre>';
+$i = 0;
+
+$word1 = [];
+$word2 = [];
+
+foreach ($countries as $country => $animals) {
+    foreach ($animals as $animal) {
+        if (substr_count($animal, " ") == 1) {
+            list($word1[], $word2[]) = explode(' ', $animal);
+        }
+    }
+}
+
+shuffle($word2);
+
+$result = implode(', ', $word2);
+echo '<pre>';
+$word2 = explode(' ', $result);
+
+foreach ($countries as $country => $animals) {
+    echo "<h2>$country</h2>";
+    foreach ($animals as $animal) {
+        if (substr_count($animal, " ") == 1) {
+            echo $word1[$i] . " ";
+            echo $word2[$i] . " ";
+            $i++;
+        }
+    }
 }
