4afa1c971cefa5c7270d04566f9231d5042dee8b
RuslanMak <makarovemd@gmail.com>
2018-04-20 08:45:22 +0300

    version-9

--- index.php
+++ index.php
@@ -9,48 +9,6 @@ $countries = [
 ];
 
 echo '<pre>';
-
-$word1 = [];
-$word2 = [];
-$countries2 = [];
-
-foreach ($countries as $country => $animals) {
-    foreach ($animals as $animal) {
-        if (substr_count($animal, " ") == 1) {
-            list($word1[], $word2[]) = explode(' ', $animal);
-            list($countries2[]) = explode(' ', $country);
-        }
-    }
-}
-
-shuffle($word2);
-
-$i = 0;
-
-foreach ($word1 as $key => $word) {
-    $not_repeat = array_unique($countries2);
-    if ($not_repeat[$i] == "") {
-        echo ", ";
-    } else {
-        echo "<h2>$not_repeat[$i]</h2>";
-    }
-    $i++;
-    echo $word . " " . $word2[$key];
-}
-
-exit;
-
-echo "<br><br><br>" . "-----------------------------------VERSION-2-------------------------------";
-
-$countries = [
-    'Canada' => ['Canadian horse', 'North American beaver'],
-    'Finland' => ['Brown bear', 'Holly blue'],
-    'Iraq' => ['Chukar partridge', 'Goat'],
-    'Japan' => ['Green pheasant', 'Carp'],
-    'Mexico' => ['Golden eagle', 'Jaguar', 'Vaquita'],
-];
-
-echo '<pre>';
 $i = 0;
 
 $word1 = [];
