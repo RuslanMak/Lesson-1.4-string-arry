dacddcbdd3112ff3c4e8dcec8364e0d2ac4d6dca
RuslanMak <makarovemd@gmail.com>
2018-05-08 20:39:39 +0300

    version-11

--- index.php
+++ index.php
@@ -7,31 +7,41 @@ $countries = [
     'Japan' => ['Green pheasant', 'Carp'],
     'Mexico' => ['Golden eagle', 'Jaguar', 'Vaquita'],
 ];
-echo '<pre>';
+
 $word1 = [];
 $word2 = [];
-$countries2 = [];
+
 foreach ($countries as $country => $animals) {
     foreach ($animals as $animal) {
-        if (substr_count($animal, " ") == 1) {
-            list($word1[], $word2[]) = explode(' ', $animal);
-            list($countries2[]) = explode(' ', $country);
+        $two_animals = [];
+        $str = explode(' ', $animal);
+
+        if (count($str) == 2) {
+            $two_animals[] = $str;
+            $part1[$country][] = $str[0];
+            $part2[] = $str[1];
         }
     }
 }
-shuffle($word2);
-$i = 0;
-foreach ($word1 as $key => $word) {
-    $not_repeat = array_unique($countries2);
-    if (isset($not_repeat[$i]) == "") {
-        echo ", ";
-    } else {
-        echo "<h2>$not_repeat[$i]</h2>";
+
+shuffle($part2);
+
+$fantastic_animals = [];
+
+foreach ($part1 as $country => $animals) {
+    foreach ($animals as $animal) {
+        $fantastic_animals[$country][] = $animal . ' ' . array_shift($part2);
     }
-    $i++;
-    echo $word . " " . $word2[$key];
 }
+
+foreach ($fantastic_animals as $country => $animal) {
+    echo "<h2>$country</h2>";
+    echo (implode(", ", $animal)) . ".";
+}
+
 exit;
+
+//---- выправил ошибту в нижнем вареанте -----
 
 $countries = [
     'Canada' => ['Canadian horse', 'North American beaver'],
@@ -40,28 +50,33 @@ $countries = [
     'Japan' => ['Green pheasant', 'Carp'],
     'Mexico' => ['Golden eagle', 'Jaguar', 'Vaquita'],
 ];
+
 echo '<pre>';
-$i = 0;
 $word1 = [];
 $word2 = [];
+$countries2 = [];
+
 foreach ($countries as $country => $animals) {
     foreach ($animals as $animal) {
         if (substr_count($animal, " ") == 1) {
             list($word1[], $word2[]) = explode(' ', $animal);
+            list($countries2[]) = explode(' ', $country);
         }
     }
 }
+
 shuffle($word2);
-$result = implode(', ', $word2);
-echo '<pre>';
-$word2 = explode(' ', $result);
-foreach ($countries as $country => $animals) {
-    echo "<h2>$country</h2>";
-    foreach ($animals as $animal) {
-        if (substr_count($animal, " ") == 1) {
-            echo $word1[$i] . " ";
-            echo $word2[$i] . " ";
-            $i++;
-        }
+$i = 0;
+
+foreach ($word1 as $key => $word) {
+    $not_repeat = array_unique($countries2);
+
+    if (isset($not_repeat[$i]) == "") {
+        echo ", ";
+    } else {
+        echo "<h2>$not_repeat[$i]</h2>";
     }
+
+    $i++;
+    echo $word . " " . $word2[$key];
 }
