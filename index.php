fc45003d356c2465e7662edfdec08fe3c884f29e
RuslanMak <makarovemd@gmail.com>
2018-10-17 22:01:54 +0300

    homework2-done

--- index.php
+++ index.php
@@ -10,50 +10,6 @@ $countries = [
 
 $word1 = [];
 $word2 = [];
-
-foreach ($countries as $country => $animals) {
-    foreach ($animals as $animal) {
-        $two_animals = [];
-        $str = explode(' ', $animal);
-
-        if (count($str) == 2) {
-            $two_animals[] = $str;
-            $part1[$country][] = $str[0];
-            $part2[] = $str[1];
-        }
-    }
-}
-
-shuffle($part2);
-
-$fantastic_animals = [];
-
-foreach ($part1 as $country => $animals) {
-    foreach ($animals as $animal) {
-        $fantastic_animals[$country][] = $animal . ' ' . array_shift($part2);
-    }
-}
-
-foreach ($fantastic_animals as $country => $animal) {
-    echo "<h2>$country</h2>";
-    echo (implode(", ", $animal)) . ".";
-}
-
-exit;
-
-//---- выправил ошибту в нижнем вареанте -----
-
-$countries = [
-    'Canada' => ['Canadian horse', 'North American beaver'],
-    'Finland' => ['Brown bear', 'Holly blue'],
-    'Iraq' => ['Chukar partridge', 'Goat'],
-    'Japan' => ['Green pheasant', 'Carp'],
-    'Mexico' => ['Golden eagle', 'Jaguar', 'Vaquita'],
-];
-
-echo '<pre>';
-$word1 = [];
-$word2 = [];
 $countries2 = [];
 
 foreach ($countries as $country => $animals) {
@@ -66,17 +22,33 @@ foreach ($countries as $country => $animals) {
 }
 
 shuffle($word2);
-$i = 0;
 
-foreach ($word1 as $key => $word) {
-    $not_repeat = array_unique($countries2);
+?>
 
-    if (isset($not_repeat[$i]) == "") {
-        echo ", ";
-    } else {
-        echo "<h2>$not_repeat[$i]</h2>";
-    }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Жестокое обращение с животными</title>
+</head>
+<body>
+ 
+ <?php 
+  $i = 0;
 
-    $i++;
-    echo $word . " " . $word2[$key];
-}
+  foreach ($word1 as $key => $word) {
+      $not_repeat = array_unique($countries2);
+
+      if (isset($not_repeat[$i]) == "") {
+          echo ", ";
+      } else {
+          echo "<h2>$not_repeat[$i]</h2>";
+      }
+
+      $i++;
+      echo $word . " " . $word2[$key];
+  }
+ ?>
+  
+</body>
+</html>
